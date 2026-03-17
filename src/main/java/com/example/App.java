5073d625b3292918e4e01ae06554e8d7d862f15a
elevate-hub <cl4887017@gmail.com>
2026-03-17 21:04:33 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new ChromeDriver();
+        try{
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(2000);
+    }catch (InterruptedException e){
+    e.printStackTrace();
+    }finally{
+    driver.quit();
+    }
+    
+}
+}
+
